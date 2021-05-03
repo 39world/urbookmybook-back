1ef491ba27fdb5ed92100cdbda26764066a3c635
Sanggu <rkdtkdrn93@naver.com>
2021-05-04 00:03:47 +0900

    프로필 수정 형식 변경
    프로필 수정 형식 변경

--- src/main/java/com/bookbook/bookback/controller/UserController.java
+++ src/main/java/com/bookbook/bookback/controller/UserController.java
@@ -41,42 +41,48 @@ public class UserController {
 
     //프로필 수정  multipart date-form 데이터를 받아 수정 진행
     //이 코드는 항상 file 데이터를 포함한 데이터가 와야함. 해결 가능할까 ?
-//    @RequestMapping("/api/profile")
-//    public UserDto profileChange(@RequestPart String userData, @RequestParam MultipartFile file, HttpServletRequest httpServletRequest) {
-//        JSONObject userJson = new JSONObject(userData);
-//        //토근에서 사용자 정보 추출
-//        String token = jwtTokenProvider.resolveToken(httpServletRequest);
-//        String email = jwtTokenProvider.getUserPk(token);
-//        User member = userRepository.findByEmail(email)
-//                .orElseThrow(() -> new IllegalArgumentException("일치하는 E-MAIL이 없습니다"));
-//        String fileUrl = fileUploadService.uploadImage(file);
-//        //해당 사용자의 프로필 업데이트
-//        UserDto userDto = new UserDto(member,userJson,fileUrl);
-//        userService.update(userDto);
-//        return userDto;
-//    }
-
-    //프로필 수정 파일 데이터 제외 버전.
-    //프로필 사진이 있을 경우 파일 업로드를 진행 후 json 데이터에 반환된 파일 url을 넣어주면 사용 가능.
     @RequestMapping("/api/profile")
-    public UserDto profileChange(@RequestBody String userData, HttpServletRequest httpServletRequest) {
-        //토근에서 사용자 정보 추출
+    public UserDto profileChange(@RequestPart String userData, @RequestParam(required = false) MultipartFile file, HttpServletRequest httpServletRequest) {
         JSONObject userJson = new JSONObject(userData);
+        //토근에서 사용자 정보 추출
         String token = jwtTokenProvider.resolveToken(httpServletRequest);
         String email = jwtTokenProvider.getUserPk(token);
         User member = userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("일치하는 E-MAIL이 없습니다"));
-        //해당 사용자의 프로필 업데이트
-        UserDto userDto = new UserDto(member,userJson);
-        userService.update(userDto);
-        return userDto;
+        if(file == null){
+                UserDto userDto = new UserDto(member,userJson);
+                userService.update(userDto);
+                return userDto;
+        } else{
+            String fileUrl = fileUploadService.uploadImage(file);
+            //해당 사용자의 프로필 업데이트
+            UserDto userDto = new UserDto(member,userJson,fileUrl);
+            userService.update(userDto);
+            return userDto;
+        }
     }
-    //프로필 사진 등록 api
-    //등록된 사진의 url을 반환
-    @PostMapping("/api/upload")
-    public String uploadImage(@RequestPart MultipartFile file) {
-        return fileUploadService.uploadImage(file);
-    }
+
+//    //프로필 수정 파일 데이터 제외 버전.
+//    //프로필 사진이 있을 경우 파일 업로드를 진행 후 json 데이터에 반환된 파일 url을 넣어주면 사용 가능.
+//    @RequestMapping("/api/profile")
+//    public UserDto profileChange(@RequestBody String userData, HttpServletRequest httpServletRequest) {
+//        //토근에서 사용자 정보 추출
+//        JSONObject userJson = new JSONObject(userData);
+//        String token = jwtTokenProvider.resolveToken(httpServletRequest);
+//        String email = jwtTokenProvider.getUserPk(token);
+//        User member = userRepository.findByEmail(email)
+//                .orElseThrow(() -> new IllegalArgumentException("일치하는 E-MAIL이 없습니다"));
+//        //해당 사용자의 프로필 업데이트
+//        UserDto userDto = new UserDto(member,userJson);
+//        userService.update(userDto);
+//        return userDto;
+//    }
+//    //프로필 사진 등록 api
+//    //등록된 사진의 url을 반환
+//    @PostMapping("/api/upload")
+//    public String uploadImage(@RequestPart MultipartFile file) {
+//        return fileUploadService.uploadImage(file);
+//    }
 
     // 기능 테스트용 자체 회원가입, 로그인
     @PostMapping("/api/signup")
