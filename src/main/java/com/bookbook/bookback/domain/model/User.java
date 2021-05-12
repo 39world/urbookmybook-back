a46e13d8bc90a6bd61bae99572008deccb123a25
Your Name <you@example.com>
2021-05-12 21:50:38 +0900

    nginx

--- src/main/java/com/bookbook/bookback/domain/model/User.java
+++ src/main/java/com/bookbook/bookback/domain/model/User.java
@@ -21,6 +21,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    private String password;
     private String email;
     private String role; //ROLE_USER, ROLE_ADMIN
 
