4aa92da5bfe2da3c64c41cacb61a4d9d2b711859
Your Name <you@example.com>
2021-05-16 03:51:21 +0900

    redis error 처리

--- src/main/java/com/bookbook/bookback/BookbackApplication.java
+++ src/main/java/com/bookbook/bookback/BookbackApplication.java
@@ -10,13 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class BookbackApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "/app/config/springboot-webservice/real-application.yml";
+//    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//            + "classpath:application.yml,"
+//            + "/app/config/springboot-webservice/real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(BookbackApplication.class)
-                .properties(APPLICATION_LOCATIONS)
+//                .properties(APPLICATION_LOCATIONS)
                 .run(args);
     }
 }
