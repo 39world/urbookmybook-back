4d5fd5c41fb32640666550a937532519ce55c3e6
lovelyAlien <54395509+lovelyAlien@users.noreply.github.com>
2021-05-22 16:23:51 +0900

    Update UserController.java
    Update UserController.java

--- src/main/java/com/bookbook/bookback/controller/UserController.java
+++ src/main/java/com/bookbook/bookback/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
     private final FileUploadService fileUploadService;
 
     //Request의 Header로 넘어온 token을 쪼개어 유저정보 확인해주는 과정
-    @GetMapping("/api/usercheck")
+    @GetMapping("/api/users/usercheck")
     public ResultReturn userInfo(HttpServletRequest httpServletRequest) {
     /*
     HTTP Request의 Header로 넘어온 token을 쪼개어 누구인지 나타내주는 과정
@@ -50,7 +50,7 @@ public class UserController {
 
 
 //    //프로필 사진이 있을 경우 파일 업로드를 진행 후 json 데이터에 반환된 파일 url을 넣어주면 사용 가능.
-    @PutMapping("/api/profile")
+    @PutMapping("/api/users/profile")
     public ResultReturn profileChange(@RequestBody String userData, HttpServletRequest httpServletRequest) {
         JSONObject userJson = new JSONObject(userData);
         //토근에서 사용자 정보 추출
@@ -145,7 +145,7 @@ public class UserController {
     }
 
     // 기능 테스트용 자체 회원가입, 로그인
-    @PostMapping("/api/signup")
+    @PostMapping("/api/users/signup")
     public ResultReturn signup(@RequestBody Map<String, String> user) {
         Optional<User> member = userRepository.findByEmail(user.get("email"));
         if(member.isPresent()){
@@ -165,7 +165,7 @@ public class UserController {
 
     }
 
-    @PostMapping("/api/login")
+    @PostMapping("/api/users/login")
     public ResultReturn login(@RequestBody Map<String, String> user) {
         User member = userRepository.findByEmail(user.get("email"))
                 .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
