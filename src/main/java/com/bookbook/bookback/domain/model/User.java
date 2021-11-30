0b5302de5982dff07e9150fcc0d7b3dc6312382a
Sanggu <rkdtkdrn93@naver.com>
2021-11-30 23:40:49 +0900

    refactor: 코드 컨벤션 적용 및 git 명령어 연습
    네이버 핵데이 자바 코드 컨벤션 적용 테스트
    git bash를 이용한 git 명령어 연습

--- src/main/java/com/bookbook/bookback/domain/model/User.java
+++ src/main/java/com/bookbook/bookback/domain/model/User.java
@@ -1,16 +1,19 @@
 package com.bookbook.bookback.domain.model;
 
 import com.bookbook.bookback.domain.dto.UserDto;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -24,81 +27,80 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements UserDetails {
-    @Id // primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String username;
-    private String password;
-    private String email;
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Builder.Default
-    private List<String> roles = new ArrayList<>();
-    private String image;
-    private String town;
-//    private String address;
-    private String comment;
-    private String myBook; //추후 구체적으로 수정 필요 ex) JSON or List<String> or List<int>
-    private Double star;  //별점 업데이트 추가 필요. 업데이트 시점, 평점 내는 로직 구현
-    @Column(name = "point", nullable = true)
-    private Long point;
+	private String username;
+	private String password;
+	private String email;
+	@ElementCollection(fetch = FetchType.EAGER)
+	@Builder.Default
+	private List<String> roles = new ArrayList<>();
+	private String image;
+	private String town;
+	//    private String address;
+	private String comment;
+	private String myBook; //추후 구체적으로 수정 필요 ex) JSON or List<String> or List<int>
+	private Double star;  //별점 업데이트 추가 필요. 업데이트 시점, 평점 내는 로직 구현
+	@Column(name = "point", nullable = true)
+	private Long point;
 
+	@ElementCollection
+	private List<Long> scrapList;
 
-    @ElementCollection
-    private List<Long> scrapList;
+	// OAuth를 위해 구성한 추가 필드 2개
+	private String provider;
+	private String providerId;
 
-    // OAuth를 위해 구성한 추가 필드 2개
-    private String provider;
-    private String providerId;
+	@CreationTimestamp
+	private Timestamp createdAt;
 
-    @CreationTimestamp
-    private Timestamp createdAt;
+	//userDetail
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.roles.stream()
+			.map(SimpleGrantedAuthority::new)
+			.collect(Collectors.toList());
+	}
 
-    //userDetail
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-    }
+	@Override
+	public String getUsername() {
+		return email;
+	}
 
-    @Override
-    public String getUsername(){
-        return email;
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+	public User(UserDto userDto) {
+		this.email = userDto.getEmail();
+		this.username = userDto.getUsername();
+		this.image = userDto.getImage();
+		this.point = 0L;
+	}
 
-    public User(UserDto userDto){
-        this.email = userDto.getEmail();
-        this.username = userDto.getUsername();
-        this.image = userDto.getImage();
-        this.point = 0L;
-    }
-
-    public void update(UserDto userDto){
-        this.id = userDto.getId();
-        this.username = userDto.getUsername();
-        this.image = userDto.getImage();
-        this.town = userDto.getTown();
-        this.comment = userDto.getComment();
-    }
+	public void update(UserDto userDto) {
+		this.id = userDto.getId();
+		this.username = userDto.getUsername();
+		this.image = userDto.getImage();
+		this.town = userDto.getTown();
+		this.comment = userDto.getComment();
+	}
 }
